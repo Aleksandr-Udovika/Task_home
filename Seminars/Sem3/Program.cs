6d74474dffed5a559f17129969a846c188d01cdd
Aleksandr_Udovika <aleksandrudovika0@gmail.com>
2023-11-12 16:35:10 +0300

    les

---
+++ Seminars/Sem3/Program.cs
@@ -0,0 +1,43 @@
+﻿// int FindQuarter(int x, int y)
+// {
+//     if(x > 0 && y > 0) return 1;
+//     else if(x < 0 && y > 0) return 2;
+//     else if(x < 0 && y < 0) return 3;
+//     else if(x > 0 && y < 0) return 4;
+//     System.Console.WriteLine("The point lies on the line!");
+//     return 0;   
+// }
+
+// System.Console.Write("Input X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// int result = FindQuarter(x, y);
+// System.Console.WriteLine($"The point located at {result} quarter");
+
+
+
+
+// Напишите программу, которая принимает на 
+// вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3, 6); B (2, 1); -> 5,09
+// A (7;-5); B (1; -1); -> 7,21
+
+double FindDistance(double xa, double ya, double xb, double yb)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2)), 2); // библиотека(Math) и в ней имеются 
+    // разные функции, к примеру Sqrt - корень. Math.Pow - возводит выражение в нужную нам степень.
+    // Math.Round - округляет числа
+}
+
+System.Console.Write("Input XA: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input YA: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input XB: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input YB: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(FindDistance(xa, ya, xb, yb));
---
+++ Seminars/Sem4/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+// [1, 0, 1, 1, 0, 1, 0, 0]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue )
+// {
+//     int[] array = new int[size]; // new - выделение памяти в нашем пк, для хранения массива
+//     for (int i = 0; i < size; i++) // работая с массивами, почти всегда цикл "for"
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// System.Console.Write("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input minimal value of array element: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input maximal value of array element: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// PrintArray(CreateRandomArray(size, minValue, maxValue));
+
+
+// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(factorial(5));
+    }
+
+    // Task - 1
+    static void sumByA(int a)
+    {
+        int sum = 0;
+        for (int i = 1; i <= a; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"Sum of A: {sum}");
+    }
+
+    // Task - 2 может с 2 начать
+
+    static int factorial(int num)
+    {
+        int result = 1;
+        for (int j = 1; j <= num; j++)
+        {
+            result *= j;
+        }
+        return result;
+    }
+}
